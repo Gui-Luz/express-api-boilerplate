68afca37fe73d7bcdc9fd5254f8d3c9238978e6c
Gui-Luz <guilhermeluzsaldanha@gmail.com>
2023-04-07 13:53:39 -0300

    swagger documentation

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,8 +4,12 @@ import usersController from "../controllers/usersController.js"
 
 export const usersRouter = express.Router();
 
-usersRouter.get('/getUsers',  usersController.getUsers)
-usersRouter.get('/getUser/:id',  usersController.getUser)
+usersRouter.get('/',  usersController.getUsers)
+usersRouter.get('/:id',  usersController.getUser)
+usersRouter.post('/',  usersController.postUser)
+usersRouter.delete('/:id',  usersController.deleteUser)
+usersRouter.put('/:id',  usersController.putUser)
+
 
 usersRouter.use((err, req, res, next) => {
     res.status(400).send({ error: err.message })
